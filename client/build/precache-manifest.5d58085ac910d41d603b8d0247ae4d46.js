2891a9ee98ff0a15290406376d50ed2c920b40a9
benbousquet <bbousque@asu.edu>
2020-10-11 05:37:26 -0700

    fix(client): client now commits build

---
+++ client/build/precache-manifest.5d58085ac910d41d603b8d0247ae4d46.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02ff6d0d6b3b9d4bccc7504cde7b109a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "971b3bdd37a00373853f",
+    "url": "/static/js/2.a481f99e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a481f99e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2803730711240869acac",
+    "url": "/static/js/main.3b4ffe6c.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
